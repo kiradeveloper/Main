68861ad7f00e1a1c19c3b0924a6c57432fb06fae
Kira <bojipedia@gmail.com>
2024-03-24 08:21:21 +0700

    Upload: Push

---
+++ config/connect_log.php
@@ -0,0 +1,7 @@
+<?php
+
+$Argument = 'DRIVER={SQL Server};SERVER=203.128.90.138,1433;DATABASE=LosaLogData';
+$username = 'lsforeverweb2';
+$password = '(Sgy%//RdH`xSc4E';
+
+$Connection = odbc_connect($Argument, $username, $password);
---
+++ config/connection.php
@@ -0,0 +1,7 @@
+<?php
+
+$Argument = 'DRIVER={SQL Server};SERVER=203.128.90.138,1433;DATABASE=LosaGame';
+$username = 'lsforeverweb';
+$password = '(Sgy%//RdH`xSc4E';
+
+$Connection = odbc_connect($Argument, $username, $password);
